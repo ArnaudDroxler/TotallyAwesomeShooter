20aaf8afc66e8e01d7c59c2278ed97fb03532e0b
Loris Ceschin <ceschinlori@gmail.com>
2017-04-01 02:01:38 +0200

    Char controller, test level, and rockets

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TAS
+{
+    public class Rocket : MonoBehaviour
+    {
+        public float radius = 500f;
+        public float power = 10f;
+        public float explosiveLift = 1f;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            //Destroy(gameObject, 5);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Vector3 origin = collision.contacts[0].point;
+            Collider[] colliders = Physics.OverlapSphere(origin, radius);
+
+            foreach (Collider hit in colliders)
+            {
+                if (hit.GetComponent<CharacterControls>())
+                {                    
+                    hit.GetComponent<CharacterControls>().RocketForce(power, origin, radius, explosiveLift);
+                }
+
+            }
+
+            Destroy(gameObject);
+
+        }
+    }
+}
---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TAS
+{
+
+    public class Shoot : MonoBehaviour
+    {
+        public float fireRate = 1;
+        private float nextFire = 0;
+
+        public Rigidbody rocket;
+        public float power;
+        private GameObject rocketSpawnPoint;
+
+        // Use this for initialization
+        void Start()
+        {
+            SetInitialReferneces();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckForInput();
+        }
+
+        void SetInitialReferneces()
+        {
+            rocketSpawnPoint = GameObject.Find("RocketSpawnPoint");
+        }
+
+        void CheckForInput()
+        {
+            // Button name in inputManager
+            if (Input.GetButton("Fire1") && Time.time >= nextFire)
+            {
+                Rigidbody clone;
+                clone = Instantiate(rocket, rocketSpawnPoint.transform.position, rocketSpawnPoint.transform.rotation);
+                clone.velocity = transform.TransformDirection(Vector3.forward * power);
+
+                nextFire = Time.time + fireRate;
+            }
+        }
+
+    }
+}
